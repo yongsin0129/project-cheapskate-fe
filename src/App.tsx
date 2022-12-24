bc0d14fa281d678e421f27a5dfc42ffcb9b5248d
yongsin0129 <yongsin0129@gmail.com>
2022-12-24 11:06:25 +0800

    feat : add toggle mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import '././style/dist/app.css'
 import { TableFC } from './components/TableFC'
+import React from 'react'
+import { ColorModeContext } from './main'
+import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
 
-function App () {
+const ToggleModeButton = () => {
+  const theme = useTheme()
+  const colorMode = React.useContext(ColorModeContext)
+
   return (
-    <div className='App'>
-      <h1>project cheapskate</h1>
-      <p className='read-the-docs'>hello world</p>
-      <TableFC />
-    </div>
+    <Box>
+      {theme.palette.mode} mode
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color='inherit'
+      >
+        {theme.palette.mode === 'dark' ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </IconButton>
+    </Box>
   )
 }
 
-export default App
+export function App () {
+  return (
+    <Box
+      sx={{
+        bgcolor: 'background.default',
+        color: 'text.primary'
+      }}
+    >
+      <h1>project cheapskate</h1>
+      <ToggleModeButton />
+      <TableFC />
+    </Box>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from './App'
 import './style/dist/main.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark'
-  }
+export const ColorModeContext = React.createContext({
+  toggleColorMode: () => {}
 })
+
+const AppManager = () => {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light')
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      }
+    }),
+    []
+  )
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode
+        }
+      }),
+    [mode]
+  )
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
+
+// 層級
+// AppManager / App
+// AppManger 管理 context
+// App 管理 page and component
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <main>This app is using the dark mode</main>
-      <App />
-    </ThemeProvider>
+    <AppManager />
   </React.StrictMode>
 )
