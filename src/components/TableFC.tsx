f703e303f397031688862b28907359dad805d2ea
yongsin0129 <yongsin0129@gmail.com>
2022-12-25 08:07:14 +0800

    feat : 使用 css after 成功做出 heart icon with click function

--- src/components/TableFC.tsx
+++ src/components/TableFC.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import '../style/dist/tableFC.css'
 import Paper from '@mui/material/Paper'
 import {
   Grid,
@@ -24,15 +25,36 @@ export const TableFC: React.FC<TableFCProps> = props => {
   const [tableColumnExtensions] = useState([
     { columnName: 'title', align: 'center' },
     { columnName: 'release', align: 'center' },
-    { columnName: 'status', align: 'center' }
+    { columnName: 'status', align: 'center' },
+    { columnName: 'favorite', align: 'center' }
   ] as tableColumnExtensionsType[])
 
   const [currentPage, setCurrentPage] = useState(0)
   const [pageSize, setPageSize] = useState(10)
   const [pageSizes] = useState([10, 25, 50])
 
+  const clickHandler = (e: React.MouseEvent) => {
+    const value = window.getComputedStyle(e.target, ':after')
+    console.log('clickHandler ~ value', value.content)
+    console.log(hasUnicode2(value.content))
+    console.log(value.zIndex)
+    console.log(e.target.parentElement?.children[0].innerText)
+    console.log(e.target.parentElement?.children[1].innerText)
+  }
+
+  function hasUnicode (str) {
+    for (var i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) > 127) return true
+    }
+    return false
+  }
+
+  function hasUnicode2 (s) {
+    return /[^\u0004-\u007f]/.test(s)
+  }
+
   return (
-    <Paper>
+    <Paper onClick={clickHandler}>
       <Grid rows={tableData} columns={columns}>
         <SearchState defaultValue='' />
         <SortingState
@@ -65,7 +87,8 @@ export const TableFC: React.FC<TableFCProps> = props => {
 const columns = [
   { name: 'title', title: 'Title' },
   { name: 'release', title: 'Release Date' },
-  { name: 'status', title: 'Status' }
+  { name: 'status', title: 'Status' },
+  { name: 'favorite', title: 'Favorite' }
 ]
 
 const rows: object[] = []
