6c7ce162e53d9c8ab400a459ec1432fc9c0653c0
yongsin0129 <yongsin0129@gmail.com>
2023-01-06 21:44:55 +0800

    fix : 修正 followedMovie page 過早回傳 需要登入的錯誤訊息

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -9,14 +9,13 @@ const ProtectedRoute = () => {
   const { MeToken, setMeToken, Me, setMe } = React.useContext(MeContext)
 
   // 如果 context_Me 有值，表示有登入可繼續 next() , 否則跳轉到登入頁面
-  if (!!Me) return <Outlet />
+  if (!!MeToken) return <Outlet />
   else {
     React.useEffect(() => {
-      setHomePageState &&
-        setHomePageState({
-          isError: true,
-          message: '收藏清單是會員專屬功能，請先登入會員 !!'
-        })
+      setHomePageState!({
+        isError: true,
+        message: '收藏清單是會員專屬功能，請先登入會員 !!'
+      })
     }, [])
     return <Navigate to='/signIn' />
   }
