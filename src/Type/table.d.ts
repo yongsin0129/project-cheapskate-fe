4c57abe9d814ea8e717b30a0a863e59604de97cd
yongsin0129 <yongsin0129@gmail.com>
2023-02-10 14:22:15 +0800

    refactor : table 中的 TableProps 修正

--- src/Type/table.d.ts
+++ src/Type/table.d.ts
@@ -1,4 +1,5 @@
 import { GridProps } from '@devexpress/dx-react-grid-material-ui'
+import * as Type from '../Type'
 
 // 針對 @devexpress 套件做 type 補充，因為版本關係造成的 bugs
 // 似乎作者在 v3 版本中 ， 沒有針對 Grid 新增 children 屬性
@@ -10,7 +11,7 @@ declare module '@devexpress/dx-react-grid-material-ui' {
 
 // react 內建的 PropsWithChildren
 export type TableProps = React.PropsWithChildren<{
-  tableData: MovieDataResponsive[]
+  tableData: (Type.MovieDataResType | null)[]
 }>
 
 export interface TableColumnExtensionsType {
